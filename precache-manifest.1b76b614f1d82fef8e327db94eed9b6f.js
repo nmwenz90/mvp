1cd8977a617d696b944603d3dcb9a10c33033d07
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-09-07 17:05:23 -0700

    Updates

---
+++ precache-manifest.1b76b614f1d82fef8e327db94eed9b6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c57fb1b05e1337edbb1973d645210f",
+    "url": "/mvp/index.html"
+  },
+  {
+    "revision": "17e5aff85fcf41136bb1",
+    "url": "/mvp/static/css/2.4623426f.chunk.css"
+  },
+  {
+    "revision": "85e98a0a5bb50facd411",
+    "url": "/mvp/static/css/main.e38a2dd9.chunk.css"
+  },
+  {
+    "revision": "17e5aff85fcf41136bb1",
+    "url": "/mvp/static/js/2.a7f5f050.chunk.js"
+  },
+  {
+    "revision": "53f028bb4ad696ffc6ff6ea115d111b4",
+    "url": "/mvp/static/js/2.a7f5f050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e98a0a5bb50facd411",
+    "url": "/mvp/static/js/main.8b29c530.chunk.js"
+  },
+  {
+    "revision": "3bdc552de085019e3718",
+    "url": "/mvp/static/js/runtime-main.f43a3eed.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/mvp/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/mvp/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/mvp/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/mvp/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/mvp/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
